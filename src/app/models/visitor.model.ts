47abf6c99d0eabce7d2ab64edfc2b552f9b2fe07
Vargas Gonzalez, Patricio <Patricio_VargasGonzalez@csx.com>
2019-03-04 19:46:41 -0500

    model created

---
+++ src/app/models/visitor.model.ts
@@ -0,0 +1,11 @@
+export interface Visitor {
+    name: string;
+    lastName: string;
+    racf: string;
+    email: string;
+    qr: string;
+    checkIn: boolean;
+    acceptTerms: boolean;
+    team: string;
+    title: string;
+}
