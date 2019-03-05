50c6ed17da2242c6c64493e6add3578a53b0fd68
Vargas Gonzalez, Patricio <Patricio_VargasGonzalez@csx.com>
2019-03-04 19:48:46 -0500

    reactive form

--- src/app/registration-page/registration-page.component.ts
+++ src/app/registration-page/registration-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Visitor } from '../models/visitor.model';
 
 @Component({
   selector: 'app-registration-page',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration-page.component.scss']
 })
 export class RegistrationPageComponent implements OnInit {
-
+    registrationForm = new FormGroup({
+      firstName: new FormControl(''),
+      lastName: new FormControl(''),
+      racf: new FormControl(''),
+      title: new FormControl(''),
+      email: new FormControl(''),
+      team: new FormControl(''),
+      acceptTerms: new FormControl(''),
+    });
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    let visitor: Visitor;
+    visitor = {
+      ...this.registrationForm.value,
+      qr: '',
+      checkin: ''
+
+    };
+    console.log(visitor);
   }
 
 }
