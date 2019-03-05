a8c823df3e7732b8779d63b3f3ca74e52eea0f37
Vargas Gonzalez, Patricio <Patricio_VargasGonzalez@csx.com>
2019-03-04 21:32:21 -0500

    using qr service

--- src/app/qr-page/qr-page.component.ts
+++ src/app/qr-page/qr-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { QrService } from '../services/qr.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-qr-page',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./qr-page.component.scss']
 })
 export class QrPageComponent implements OnInit {
-
-  constructor() { }
+  elementType: 'url' | 'canvas' | 'img' = 'url';
+  QR: string;
+  constructor(private qrService: QrService, private router: Router) { }
 
   ngOnInit() {
+    this.QR = this.qrService.getQR();
+    this.qrService.setQR('');
+  }
+
+  goBack() {
+    this.router.navigate(['/']);
   }
 
 }
