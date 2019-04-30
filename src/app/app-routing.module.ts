46f5d3556d80a0b7d0e749294707e8d694157627
Vargas Gonzalez, Patricio <Patricio_VargasGonzalez@csx.com>
2019-04-30 09:28:36 -0400

    fixed all prod issues

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,45 +7,45 @@ import { QrPageComponent } from './components/qr-page/qr-page.component';
 import { WelcomePageComponent } from './components/welcome-page/welcome-page.component';
 import { InfoPageComponent } from './components/info-page/info-page.component';
 import { HackActionsComponent } from './components/hack-actions/hack-actions.component';
-import { TermsConditionsComponent } from './terms-conditions/terms-conditions.component';
+import { TermsConditionsComponent } from './components/terms-conditions/terms-conditions.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: HomePageComponent
-  },
-  {
-    path: 'registration',
-    component: RegistrationPageComponent
-  },
-  {
-    path: 'checkin',
-    component: CheckinPageComponent
-  },
-  {
-    path: 'qr',
-    component: QrPageComponent
-  },
-  {
-    path: 'welcome',
-    component: WelcomePageComponent
-  },
-  {
-    path: 'participants',
-    component: InfoPageComponent
-  },
-  {
-    path: 'actions',
-    component: HackActionsComponent
-  },
-  {
-    path: 'terms&conditions',
-    component: TermsConditionsComponent
-  }
+	{
+		path: '',
+		component: HomePageComponent
+	},
+	{
+		path: 'registration',
+		component: RegistrationPageComponent
+	},
+	{
+		path: 'checkin',
+		component: CheckinPageComponent
+	},
+	{
+		path: 'qr',
+		component: QrPageComponent
+	},
+	{
+		path: 'welcome',
+		component: WelcomePageComponent
+	},
+	{
+		path: 'participants',
+		component: InfoPageComponent
+	},
+	{
+		path: 'actions',
+		component: HackActionsComponent
+	},
+	{
+		path: 'terms&conditions',
+		component: TermsConditionsComponent
+	}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [ RouterModule.forRoot(routes) ],
+	exports: [ RouterModule ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,28 +13,27 @@ import { QrPageComponent } from './components/qr-page/qr-page.component';
 import { CheckinPageComponent } from './components/checkin-page/checkin-page.component';
 import { WelcomePageComponent } from './components//welcome-page/welcome-page.component';
 
-import {ButtonModule} from 'primeng/button';
-import {MatButtonModule} from '@angular/material/button';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatExpansionModule} from '@angular/material/expansion';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
-import {MatPaginatorModule} from '@angular/material/paginator';
-import {MatSortModule} from '@angular/material/sort';
-import {MatTableModule} from '@angular/material/table';
-import {MatIconModule} from '@angular/material/icon';
-import {MatDialogModule} from '@angular/material/dialog';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import { ButtonModule } from 'primeng/button';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { NgxQRCodeModule } from 'ngx-qrcode2';
 import { ZXingScannerModule } from '@zxing/ngx-scanner';
 import { InfoPageComponent, QRDialogComponent } from './components/info-page/info-page.component';
 import { HackActionsComponent } from './components/hack-actions/hack-actions.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-
 
 import { TeamsComponent } from './components/teams/teams.component';
 import { SponsorsComponent } from './components/sponsors/sponsors.component';
@@ -42,59 +41,58 @@ import { AboutComponent } from './components/about/about.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { RegistrationComponent } from './components/registration/registration.component';
-import { TermsConditionsComponent } from './terms-conditions/terms-conditions.component';
-import { TimelineComponent } from './timeline/timeline.component';
-
+import { TermsConditionsComponent } from './components/terms-conditions/terms-conditions.component';
+import { TimelineComponent } from './components/timeline/timeline.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomePageComponent,
-    RegistrationPageComponent,
-    QrPageComponent,
-    CheckinPageComponent,
-    WelcomePageComponent,
-    InfoPageComponent,
-    QRDialogComponent,
-    HackActionsComponent,
-    NavbarComponent,
-    TeamsComponent,
-    SponsorsComponent,
-    AboutComponent,
-    HeaderComponent,
-    ContactComponent,
-    RegistrationComponent,
-    TermsConditionsComponent,
-    TimelineComponent
-  ],
-  entryComponents: [QRDialogComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    ButtonModule,
-    MatButtonModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatExpansionModule,
-    MatCheckboxModule,
-    ReactiveFormsModule,
-    NgxQRCodeModule,
-    MatSnackBarModule,
-    ZXingScannerModule,
-    HttpClientModule,
-    MatIconModule,
-    MatTableModule,
-    MatPaginatorModule,
-    MatSortModule,
-    MatToolbarModule,
-    MatProgressSpinnerModule,
-    FormsModule,
-    MatDialogModule,
-    FlexLayoutModule,
-    MatMenuModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		HomePageComponent,
+		RegistrationPageComponent,
+		QrPageComponent,
+		CheckinPageComponent,
+		WelcomePageComponent,
+		InfoPageComponent,
+		QRDialogComponent,
+		HackActionsComponent,
+		NavbarComponent,
+		TeamsComponent,
+		SponsorsComponent,
+		AboutComponent,
+		HeaderComponent,
+		ContactComponent,
+		RegistrationComponent,
+		TermsConditionsComponent,
+		TimelineComponent
+	],
+	entryComponents: [ QRDialogComponent ],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		BrowserAnimationsModule,
+		ButtonModule,
+		MatButtonModule,
+		MatFormFieldModule,
+		MatInputModule,
+		MatExpansionModule,
+		MatCheckboxModule,
+		ReactiveFormsModule,
+		NgxQRCodeModule,
+		MatSnackBarModule,
+		ZXingScannerModule,
+		HttpClientModule,
+		MatIconModule,
+		MatTableModule,
+		MatPaginatorModule,
+		MatSortModule,
+		MatToolbarModule,
+		MatProgressSpinnerModule,
+		FormsModule,
+		MatDialogModule,
+		FlexLayoutModule,
+		MatMenuModule
+	],
+	providers: [],
+	bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
