7467be22fa3a1271515827ddf0f2f88c80c7decf
Vargas Gonzalez, Patricio <Patricio_VargasGonzalez@csx.com>
2019-04-30 22:46:07 -0400

    changed color of buttons and added small logos

--- src/app/components/welcome-page/welcome-page.component.ts
+++ src/app/components/welcome-page/welcome-page.component.ts
@@ -2,22 +2,20 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-welcome-page',
-  templateUrl: './welcome-page.component.html',
-  styleUrls: ['./welcome-page.component.scss']
+	selector: 'app-welcome-page',
+	templateUrl: './welcome-page.component.html',
+	styleUrls: [ './welcome-page.component.scss' ]
 })
 export class WelcomePageComponent implements OnInit {
+	constructor(private router: Router) {}
 
-  constructor(private router: Router) { }
+	ngOnInit(): void {
+		// this.takeMeBack();
+	}
 
-  ngOnInit(): void {
-    this.takeMeBack();
-  }
-
-  takeMeBack(): void {
-    setTimeout(() => {
-      this.router.navigate(['/']);
-    }, 3000);
-  }
-
+	takeMeBack(): void {
+		setTimeout(() => {
+			this.router.navigate([ '/' ]);
+		}, 3000);
+	}
 }
