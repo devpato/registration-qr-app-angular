0961ee5ba09125f41a058f66419369d414a05b64
Vargas Gonzalez, Patricio <Patricio_VargasGonzalez@csx.com>
2019-04-30 23:17:47 -0400

    enabled/disabled link

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+	selector: 'app-navbar',
+	templateUrl: './navbar.component.html',
+	styleUrls: [ './navbar.component.scss' ]
 })
 export class NavbarComponent implements OnInit {
+	constructor() {}
 
-  constructor() { }
+	ngOnInit() {}
 
-  ngOnInit() {
-  }
-
+	dayOf() {
+		return (
+			new Date().getMonth() + '/' + new Date().getDay() ===
+			new Date('17/04/2019').getMonth() + '/' + new Date('17/04/2019').getDay()
+		);
+	}
 }
